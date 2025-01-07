07df706d45e1ea9721d2be5cd8066863a029a672
Alena Timofeeva <alenatimofeeva@Alenas-MacBook-Air.local>
2025-01-07 14:20:15 -0800

    00: setup Next.js project

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import { Lora } from "next/font/google";
+
+const LoraFont = Lora({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div
+      className={`${LoraFont.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h2>Ателье "Танюша"</h2>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      </footer>
+    </div>
+  );
+}
