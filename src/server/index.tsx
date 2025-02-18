16f229fce694a38de2423b1d99801a04501609cd
Alena Timofeeva <mouse.programmouse@gmail.com>
2025-02-17 19:57:06 -0800

    Revert language and update alt lang links in header

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -34,9 +34,9 @@ app.get("*", (req, res) => {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="alternate" hreflang="ua" href="https://${i18n.t("domain")}/ua" />
-        <link rel="alternate" hreflang="ru" href="https://${i18n.t("domain")}/ru" />
-        <link rel="alternate" hreflang="x-default" href="https://${i18n.t("domain")}/ua" />
+        <link rel="alternate" hreflang="uk" href="https://${process.env.REACT_APP_DOMAIN}/uk" />
+        <link rel="alternate" hreflang="ru" href="https://${process.env.REACT_APP_DOMAIN}/ru" />
+        <link rel="alternate" hreflang="x-default" href="https://${process.env.REACT_APP_DOMAIN}" />
         <meta name="robots" content="index, follow" />
         <meta
           name="description"
